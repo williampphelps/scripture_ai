ed4a8e14eaf663d27218c5544977f88d88111f5c
William Phelps <william@Mac.localdomain>
2025-06-19 05:03:13 -0600

    Add error checking

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,64 +7,82 @@
 	let num_results = $state(10);
 	let chat_answers = $state('# Enter your question below:');
 
+	type Verse = {
+		id: string;
+		bookName: string;
+		chapterName: string;
+		number: number;
+		text: string;
+	};
+
 	async function getScriptures(e: Event) {
 		e.preventDefault();
-		chat_answers = '# Finding relevant verses... \n\n This may take a few seconds...';
-		let searchResponse = await fetch('/api/ai', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				query: search_query,
-				num_results: num_results
-			})
-		});
-		const searchData = await searchResponse.json();
+		try {
+			chat_answers = '# Finding relevant verses... \n\n This may take a few seconds...';
+			let searchResponse = await fetch('/api/ai', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					query: search_query,
+					num_results: num_results
+				})
+			});
 
-		console.log(searchData);
+			// code not 200:
+			//
 
-		// create new array by grouping verses by chapter and then sort by verse number
-
-		const groupedVerses = searchData.verses.reduce((acc, verse) => {
-			const key = `${verse.bookName} ${verse.chapterName}`;
-			if (!acc[key]) acc[key] = [];
-			acc[key].push(verse);
-			return acc;
-		}, {});
-
-		const sortedVerses = Object.values(groupedVerses).map((chapterVerses) => {
-			return chapterVerses.sort((a, b) => a.number - b.number);
-		});
-
-		console.log(sortedVerses);
-
-		let new_answers = '# ' + search_query + '\n\n';
-
-		for (let i = 0; i < sortedVerses.length; i++) {
-			const verses = sortedVerses[i];
-			const numbers = verses.map((verse) => verse.number).join(',');
-			new_answers += '---\n\n## ' + verses[0].bookName + ' ' + verses[0].chapterName + '\n\n';
-
-			new_answers += '*' + verses[0].chapterSummary + '*\n\n';
-			for (let j = 0; j < verses.length; j++) {
-				new_answers += `${verses[j].content.replace(/\[\^.*?\]/g, '')}\n\n`;
+			if (!searchResponse.ok) {
+				throw new Error(`HTTP error: status: ${searchResponse.status}`);
 			}
-			new_answers +=
-				'<a href="https://www.churchofjesuschrist.org/study/scriptures/' +
-				verses[0].workSlug +
-				'/' +
-				verses[0].bookSlug +
-				'/' +
-				verses[0].chapterSlug +
-				'&id=p' +
-				numbers +
-				'#p' +
-				(verses[0].number - 1) +
-				'" target="_blank">View on ChurchOfJesusChrist.org</a>\n\n';
-			new_answers += '\n\n';
+
+			const searchData = await searchResponse.json();
+
+			// create new array by grouping verses by chapter and then sort by verse number
+
+			const groupedVerses = searchData.verses.reduce((acc: [], verse: Verse) => {
+				const key = `${verse.bookName} ${verse.chapterName}`;
+				if (!acc[key]) acc[key] = [];
+				acc[key].push(verse);
+				return acc;
+			}, {});
+
+			const sortedVerses = Object.values(groupedVerses).map((chapterVerses) => {
+				return chapterVerses.sort((a, b) => a.number - b.number);
+			});
+
+			let new_answers = '# ' + search_query + '\n\n';
+
+			for (let i = 0; i < sortedVerses.length; i++) {
+				const verses = sortedVerses[i];
+				const numbers = verses.map((verse: Verse) => verse.number).join(',');
+				new_answers += '---\n\n## ' + verses[0].bookName + ' ' + verses[0].chapterName + '\n\n';
+
+				new_answers += '*' + verses[0].chapterSummary + '*\n\n';
+				for (let j = 0; j < verses.length; j++) {
+					new_answers += `${verses[j].content.replace(/\[\^.*?\]/g, '')}\n\n`;
+				}
+				new_answers +=
+					'<a href="https://www.churchofjesuschrist.org/study/scriptures/' +
+					verses[0].workSlug +
+					'/' +
+					verses[0].bookSlug +
+					'/' +
+					verses[0].chapterSlug +
+					'&id=p' +
+					numbers +
+					'#p' +
+					(verses[0].number - 1) +
+					'" target="_blank">View on ChurchOfJesusChrist.org</a>\n\n';
+				new_answers += '\n\n';
+			}
+			chat_answers = new_answers;
+			search_query = '';
+		} catch (error) {
+			console.error(error);
+			chat_answers = '# Error fetching verses';
 		}
-		chat_answers = new_answers;
 	}
 </script>
 
@@ -84,10 +102,18 @@
 			name="search_query"
 			placeholder="Enter question here... (example: What is faith?)"
 			class="input w-xl max-w-full shrink"
+			required
 		/>
 		<div class="flex flex-col gap-2">
 			<label class="input w-full sm:w-52" for="num_results">
-				<input type="number" bind:value={num_results} min="1" name="num_results" />
+				<input
+					type="number"
+					bind:value={num_results}
+					min="1"
+					max="1000"
+					name="num_results"
+					required
+				/>
 				<span class="label">results</span>
 			</label>
 		</div>
--- src/routes/api/ai/+server.ts
+++ src/routes/api/ai/+server.ts
@@ -10,37 +10,61 @@ export const POST: RequestHandler = async ({ request }) => {
 
   const { query, num_results } = await request.json();
 
-  const embedQuery = "search_query: " + query;
+  // ensure query is not empty
+  if (!query) {
+    return new Response('Query cannot be empty', { status: 400 });
+  }
 
-  const embeddingsResponse = await fetch(OLLAMA_URL + "/api/embeddings", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      model: "nomic-embed-text",
-      prompt: embedQuery,
-      stream: false
-    })
-  });
-  const embeddingsData = await embeddingsResponse.json();
 
-  await db.connect(SURREAL_URL + '/rpc', {
-    namespace: SURREAL_NS,
-    database: SURREAL_DB,
-    auth: {
-      username: SURREAL_USERNAME,
-      password: SURREAL_PASSWORD,
-      namespace: SURREAL_NS
-    }
-  });
+  // ensure num_results is an integer and is greater than 0 and less than 1000
+  if (!Number.isInteger(num_results) || num_results <= 0 || num_results > 1000) {
+    return new Response('Invalid num_results', { status: 400 });
+  }
 
-  const result = await db.query("SELECT id, chapter.name as chapterName, chapter.book.name as bookName, chapter.summary as chapterSummary, chapter, content, number, vector::similarity::cosine(embeddings.nomicEmbedTextPrefix, $query_embeddings) as similarity, chapter.slug as chapterSlug, chapter.book.slug as bookSlug, chapter.book.work.slug as workSlug FROM verse WHERE embeddings.nomicEmbedTextPrefix <|" + num_results + "|> $query_embeddings ORDER BY similarity DESC;", {
-    query_embeddings: embeddingsData.embedding
-  });
+  // sanitize query
+  const sanitizedQuery = query.replace(/[^a-zA-Z0-9\s]/g, '');
 
-  // console.log(result);
 
-  return json({ verses: result[0] });
+
+  const embedQuery = "search_query: " + sanitizedQuery;
+
+
+  try {
+    const embeddingsResponse = await fetch(OLLAMA_URL + "/api/embeddings", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        model: "nomic-embed-text",
+        prompt: embedQuery,
+        stream: false
+      })
+    });
+    const embeddingsData = await embeddingsResponse.json();
+
+    await db.connect(SURREAL_URL + '/rpc', {
+      namespace: SURREAL_NS,
+      database: SURREAL_DB,
+      auth: {
+        username: SURREAL_USERNAME,
+        password: SURREAL_PASSWORD,
+        namespace: SURREAL_NS
+      }
+    });
+
+    const result = await db.query("SELECT id, chapter.name as chapterName, chapter.book.name as bookName, chapter.summary as chapterSummary, chapter, content, number, vector::similarity::cosine(embeddings.nomicEmbedTextPrefix, $query_embeddings) as similarity, chapter.slug as chapterSlug, chapter.book.slug as bookSlug, chapter.book.work.slug as workSlug FROM verse WHERE embeddings.nomicEmbedTextPrefix <|" + num_results + "|> $query_embeddings ORDER BY similarity DESC;", {
+      query_embeddings: embeddingsData.embedding
+    });
+
+    // console.log(result);
+
+    return json({ verses: result[0] });
+  } catch (e) {
+    console.error(e);
+    return new Response('An error occurred', { status: 500 });
+  }
+
+
 
 }
